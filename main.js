48927b055acda0620bcdb05808909b3bdac8d11e
StudioMollan <61665234+StudioMollan@users.noreply.github.com>
2020-12-18 23:59:33 +0100

    more fix

--- main.js
+++ main.js
@@ -522,7 +522,7 @@ function ValidateLogin(name, pass) {
   var i;
   var correct;
   for (i = 0; i < userArray.length; i++) {
-    if (name == userArray[i].username) {
+    if (name + pass== userArray[i].username+userArray[i].password) {
       correct = true;
     }
   }
@@ -600,8 +600,8 @@ function newUserShow() {
         loginForm.appendChild(loginPasswordInput);
         // submit button
         var loginSubmitButton = document.createElement("button");
-        loginSubmitButton.setAttribute("id","user-login");
-        loginSubmitButton.setAttribute("name","login-user");
+        loginSubmitButton.setAttribute("id","new-user");
+        loginSubmitButton.setAttribute("name","submit-user");
         loginSubmitButton.setAttribute("type","button");
         var logSubmitText = document.createTextNode("Create");
         loginSubmitButton.appendChild(logSubmitText);
